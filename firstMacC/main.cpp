184a4357e7f11b41f9b96a075c2d045140b1c643
Kevin Coyle <kevin.coyle@drutek.co.uk>
2014-02-22 22:35:04 +0000

    Initial Commit

---
+++ firstMacC/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  firstMacC
+//
+//  Created by Kevin Coyle on 22/02/2014.
+//  Copyright (c) 2014 Kevin Coyle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
